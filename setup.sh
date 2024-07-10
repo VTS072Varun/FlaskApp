b14af6b7dd07b2e0b3a4b0a1547829ea14511038
VTS072Varun <varun@vivatechsolutions.io>
2024-07-10 17:12:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-cd /home/ec2-user/Flask-App
+
+# Update the package list
+sudo yum update -y
+
+# Install necessary packages
+sudo yum install -y python3 python3-pip
+
+# Create a virtual environment
 python3 -m venv venv
+
+# Activate the virtual environment
 source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
