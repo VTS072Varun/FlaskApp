3ba9553726b2bdb71e217e75a0ec806d168cc82c
VTS072Varun <varun@vivatechsolutions.io>
2025-02-27 19:39:32 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+docker pull vivatechsolution/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5001:5001 vivatechsolution/simple-python-flask-app
