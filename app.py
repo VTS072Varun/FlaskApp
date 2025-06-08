72170fec936a9a67e8d57f4905ae50a1e54f323b
VTS072Varun <varun@vivatechsolutions.io>
2025-06-09 02:06:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello from EC2 via GitHub Actions!'
 
 if __name__ == '__main__':
-    app.run()
-
-
-
-
-
+    app.run(host='0.0.0.0', port=8000)
