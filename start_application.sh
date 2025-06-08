c8fbc0baa85ae0ea41d26f21c9bf564ead140bfa
VTS072Varun <varun@vivatechsolutions.io>
2025-06-09 03:30:33 +0530

    Update start_application.sh

--- start_application.sh
+++ start_application.sh
@@ -5,7 +5,7 @@ docker stop githubactionsdemo || true
 docker rm githubactionsdemo || true
 
 # Pull the latest image
-docker pull $DOCKER_USERNAME/githubactionsdemo:latest
+docker pull varuntej18/githubactionsdemo:latest
 
-# Run new container
-docker run -d --name githubactionsdemo -p 8000:8000 $DOCKER_USERNAME/githubactionsdemo:latest
+# Run new container on port 8000
+docker run -d --name githubactionsdemo -p 8000:8000 varuntej18/githubactionsdemo:latest
